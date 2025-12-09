dd9a258b4aa08bd25e1b725c355707919575ee46
William de Lima <williamdelimacx@gmail.com>
2025-12-09 09:36:48 -0300

    chore: add a text component with cva

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-red-500">Hello World</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Hello World
+      </Text>
+      <Text className="text-green-base">Hello World</Text>
+      <Text variant="body-md-bold">Hello World</Text>
+      <Text>Test</Text>
+    </div>
+  );
 }
