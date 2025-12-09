d13feab24de4580b08695892374570b6c72ce6e8
William de Lima <williamdelimacx@gmail.com>
2025-12-09 13:34:50 -0300

    feat: add container component

--- src/App.tsx
+++ src/App.tsx
@@ -13,54 +13,57 @@ import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input.text";
 import InputCheckbox from "./components/input-checkbox";
 import Card from "./components/card";
+import Container from "./components/container";
 
 export default function App() {
   return (
-    <div className="grid gap-10">
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold" className="text-pink-base">
-          Hello World
-        </Text>
-        <Text className="text-green-base">Hello World</Text>
-        <Text variant="body-md-bold">Hello World</Text>
-        <Text>Test</Text>
-      </div>
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Hello World
+          </Text>
+          <Text className="text-green-base">Hello World</Text>
+          <Text variant="body-md-bold">Hello World</Text>
+          <Text>Test</Text>
+        </div>
 
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} className="fill-green-base" />
-        <Icon svg={CheckIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={XIcon} />
-      </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={XIcon} />
+        </div>
 
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 5</Badge>
-      </div>
+        <div>
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+        </div>
 
-      <div>
-        <Button icon={PlusIcon}>New Task</Button>
-      </div>
+        <div>
+          <Button icon={PlusIcon}>New Task</Button>
+        </div>
 
-      <div className="flex grap-1">
-        <ButtonIcon icon={TrashIcon} disabled />
-        <ButtonIcon icon={TrashIcon} variant="secondary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
+        <div className="flex grap-1">
+          <ButtonIcon icon={TrashIcon} disabled />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
 
-      <div>
-        <InputText />
-      </div>
+        <div>
+          <InputText />
+        </div>
 
-      <div>
-        <InputCheckbox />
-      </div>
+        <div>
+          <InputCheckbox />
+        </div>
 
-      <div>
-        <Card size="md">Hello World</Card>
+        <div>
+          <Card size="md">Hello World</Card>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
