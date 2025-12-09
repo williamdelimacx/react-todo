28472e1778bbfd34b6a4a8a4199e343a0f3a0e0d
William de Lima <williamdelimacx@gmail.com>
2025-12-09 15:35:52 -0300

    feat: set localstorage to db

--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -2,8 +2,13 @@ import Button from "../components/button";
 
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksList() {
+  const { tasks } = useTasks();
+
+  console.log(tasks);
+
   return (
     <>
       <section>
