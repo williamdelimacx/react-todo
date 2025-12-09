378a05cb746745fab5c643f53e452d10f080a8e1
William de Lima <williamdelimacx@gmail.com>
2025-12-09 11:42:31 -0300

    feat: add badge component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PencilIcon from "./assets/icons/pencil.svg?react";
 import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import Icon from "./components/icon";
+import Badge from "./components/badge";
 
 export default function App() {
   return (
@@ -27,6 +28,11 @@ export default function App() {
         <Icon svg={SpinnerIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </div>
   );
