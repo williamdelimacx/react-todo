13d577d69980aa6ecd135857de201a48941674d7
William de Lima <williamdelimacx@gmail.com>
2025-12-15 10:36:28 -0300

    feat: updating task status

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -22,7 +22,7 @@ export default function TaskItem({ task }: TaskItemProps) {
     task?.state === TaskState.Creating
   );
   const [taskTitle, setTaskTitle] = React.useState(task.title || "");
-  const { updateTask } = useTask();
+  const { updateTask, updateTaskStatus } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -44,13 +44,19 @@ export default function TaskItem({ task }: TaskItemProps) {
     setIsEditing(false);
   }
 
+  function handleChangeTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+    const checked = e.target.checked;
+
+    updateTaskStatus(task.id, checked);
+  }
+
   return (
     <Card size="md">
       {!isEditing ? (
         <div className="flex items-center gap-4">
           <InputCheckbox
-            value={task.concluded?.toString()}
             checked={task.concluded}
+            onChange={handleChangeTaskStatus}
           />
           <Text className={cx("flex-1", { "line-through": task?.concluded })}>
             {task?.title}
