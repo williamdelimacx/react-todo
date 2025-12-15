82da2cc239815b56ea154535d828b559ac7f0509
William de Lima <williamdelimacx@gmail.com>
2025-12-14 21:42:52 -0300

    feat: preparing task

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -3,16 +3,22 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
 
 export default function TasksList() {
   const { tasks } = useTasks();
+  const { prepareTask } = useTask();
 
   console.log(tasks);
+
+  function handleNewTask() {
+    prepareTask();
+  }
 
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full">
+        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
           Nova tarefa
         </Button>
       </section>
